eea293c99aaaad24faabef99933bb20675a1d593
Maëlle MANIFACIER <m.manifacier@w3plus.fr>
2021-03-11 09:32:00 +0100

    stars rotation

--- lis-navi/src/app/features/accueil/pages/accueil/accueil.page.ts
+++ lis-navi/src/app/features/accueil/pages/accueil/accueil.page.ts
@@ -16,12 +16,21 @@ export class AccueilPage implements OnInit {
 
   ngOnInit(): void {
     this.stars?.push(new Star('etoile-filante', `1vh`, `10vw`, '0', '0'));
-    for(let i = 0; i < 150; i++) {
-      this.stars?.push(new Star('starx1', `${Math.floor(Math.random() * 100)}vh`, `${i}vw`, '0', '0'));
+    let angle = Math.random() * Math.PI * 2;
+    const radius = 15000;
+    let rad = Math.random() * radius;
+    for(let i = 0; i < 200; i++) {
+      this.stars?.push(new Star('starx1', `${Math.cos(angle) * Math.sqrt(rad)}vh`, `${Math.sin(angle) * Math.sqrt(rad)}vw`, '0', '0'));
+      angle = Math.random() * Math.PI * 2;
+      rad = Math.random() * radius;
     }
-    for(let i = 0; i < 20; i++) {
-      this.stars?.push(new Star('starx2', `${Math.floor(Math.random() * 100)}vh`, `${Math.floor(Math.random() * 100)}vw`, '0', '0'));
+    for(let i = 0; i < 50; i++) {
+      // `${Math.floor(Math.random() * 100)}vh`
+      this.stars?.push(new Star('starx2', `${Math.cos(angle) * Math.sqrt(rad)}vh`, `${Math.sin(angle) * Math.sqrt(rad)}vw`, '0', '0'));
+      angle = Math.random() * Math.PI * 2;
+      rad = Math.random() * radius;
     }
+    console.log(this.stars)
   }
 
 }
